1e8109bbfb7ce1a3d94f68783c2ec9edff19700d
Mohan Raj D <mohanrajdilli3@gmail.com>
2025-05-26 00:44:07 +0530

    Add files via upload

---
+++ patterns/patterns 9.py
@@ -0,0 +1,11 @@
+n=int(5)
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+    
+for k in range(1,n):
+    for l in range(n-k):
+         print("*",end=" ")
+    print("")
